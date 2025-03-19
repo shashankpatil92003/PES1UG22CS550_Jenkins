7c14b6ef975d7a5491bb703f567468c1a433090d
shashankpatil92003 <166741391+shashankpatil92003@users.noreply.github.com>
2025-03-19 12:35:28 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins\n";//printing hello Jenkins
   cout << "I have successfully built and run this  \n";
   return 0;
 }
